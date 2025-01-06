7d38104e658524110822d826e4cb7154559d326f
Mushriq Al-Jazrawe <mushriq@mit.edu>
2025-01-05 20:00:05 -0500

    Updated package

---
+++ R/test.R
@@ -0,0 +1,15 @@
+#' Test a number
+#'
+#' This is a test function that takes a number
+#' and doubles it.
+#'
+#' @param x The number
+#' @return The number doubled
+#' @export
+test <- function(x){
+
+  result <- x * 2
+
+  print(result)
+
+}
